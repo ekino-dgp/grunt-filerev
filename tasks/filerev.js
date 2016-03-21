3aa9d489341ace20c278e69a8cad55f6e0fc242f
ekino-dgp <ekino-dgp@users.noreply.github.com>
2016-03-21 16:26:34 +0100

    expose srcDirname to process option so that we can genereate a relative output path
    Update README.md

--- tasks/filerev.js
+++ tasks/filerev.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
           var stat = fs.lstatSync(el.dest);
 
           if (stat && !stat.isDirectory()) {
-            grunt.fail.fatal('Destination ' + el.dest  + ' for target ' + target + ' is not a directory');
+            grunt.fail.fatal('Destination ' + el.dest + ' for target ' + target + ' is not a directory');
           }
         } catch (err) {
           grunt.verbose.writeln('Destination dir ' + el.dest + ' does not exists for target ' + target + ': creating');
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
         var newName;
 
         if (typeof options.process === 'function') {
-          newName = options.process(path.basename(file, ext), suffix, ext.slice(1));
+          newName = options.process(path.basename(file, ext), suffix, ext.slice(1), path.dirname(file));
         } else {
           if (options.process) {
             grunt.log.error('options.process must be a function; ignoring');
